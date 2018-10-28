840b59dec5ed1d77923ebe40ae69a09918d9f966
Giuseppe Milicia <gppmilicia@gmail.com>
2018-10-28 20:47:27 +0100

    Added venv to the repo added bash script to run the python recommendation added output to the python script

--- recommendation.py
+++ recommendation.py
@@ -8,25 +8,25 @@ from scipy.spatial.distance import cosine
 
 warn.filterwarnings('ignore')
 
-# Read CSV with signals
+print('Read CSV with signals')
 df = pd.read_csv('./signals/21B_tag_views_dataset.csv', encoding='latin-1')
 df.head()
 
-# Create DataFrame with only user_id and tag_id column
+print('Create DataFrame with only user_id and tag_id column')
 user_tag_view = pd.DataFrame(df, columns=['user_id', 'tag_id'])
 user_tag_view.head()
 
-# Count unique users and tags are in the sample
+print('Count unique users and tags are in the sample')
 n_users = df.user_id.unique().shape[0]
 n_tags = df.tag_id.unique().shape[0]
 print(
     'Number of users = ' + str(n_users) + ' | Number of tags = ' + str(n_tags))
 
-# add column of action with value 1
+print('Add column of action with value 1')
 user_tag_view['view'] = '1'
 user_tag_view.head()
 
-# Create Pivot table
+print('Create Pivot table')
 viewPivot = user_tag_view.pivot_table(index=['user_id'],
                                       columns=['tag_id'],
                                       values='view',
@@ -34,16 +34,16 @@ viewPivot = user_tag_view.pivot_table(index=['user_id'],
                                       fill_value=0)
 viewPivot.head()
 
-# Reindex DataFrame to remove the user data
+print('Reindex DataFrame to remove the user data')
 viewPivot.reset_index(inplace=True)
 viewPivot = viewPivot.drop('user_id', axis=1)
 viewPivot.head()
 
-# Create temp matrix to store the similarity
+print('Create temp matrix to store the similarity')
 tmpViewMatrix = pd.DataFrame(index=viewPivot.columns, columns=viewPivot.columns)
 tmpViewMatrix.head()
 
-# Start calculating the similarity
+print('Start calculating the similarity')
 print(dt.now().strftime("%Y-%m-%d %H:%M"))
 
 for i in range(0, len(tmpViewMatrix.columns)):
@@ -54,7 +54,7 @@ tmpViewMatrix.head()
 
 print(dt.now().strftime("%Y-%m-%d %H:%M"))
 
-# Create DataFrame with ordered tags by similarity
+print('Create DataFrame with ordered tags by similarity')
 similarTags = pd.DataFrame(index=tmpViewMatrix.columns,
                            columns=range(1, len(tmpViewMatrix.columns)))
 for i in range(0, len(tmpViewMatrix.columns)):
@@ -62,11 +62,11 @@ for i in range(0, len(tmpViewMatrix.columns)):
                                                        0:, i].sort_values(
     ascending=False)[:len(tmpViewMatrix.columns) - 1].index
 
-# Top 10 tags with their recommended tags
+print('Top 10 tags with their recommended tags')
 topSimilarTags = similarTags.head(10).ix[:10, 2:11]
 
 print(dt.now().strftime("%Y-%m-%d %H:%M"))
 
-# Save data to csv to use in your own recommendation system
+print('Save data to csv to use in your own recommendation system')
 tmpViewMatrix.to_csv('./results/temp_matrix_result.csv')
 topSimilarTags.to_csv('./results/similar_result.csv')
