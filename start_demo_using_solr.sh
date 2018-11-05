2a6122ba06b7bfe200b5b8729465e41794179464
Giuseppe Milicia <gppmilicia@gmail.com>
2018-11-05 11:34:53 +0100

    Renamed docker containers

--- start_demo_using_solr.sh
+++ start_demo_using_solr.sh
@@ -7,18 +7,18 @@ docker-compose start
 sleep 10
 
 # Create database
-docker exec -it mysql /bin/bash -c "mysql -uroot -proot -e 'CREATE DATABASE IF NOT EXISTS test_21b;'"
+docker exec -it mysql-test-mil /bin/bash -c "mysql -uroot -proot -e 'CREATE DATABASE IF NOT EXISTS test_21b;'"
 # Create table and load signals data to DB
-docker exec -it mysql /bin/bash -c "mysql -uroot -proot test_21b < /script/load_data.sql"
+docker exec -it mysql-test-mil /bin/bash -c "mysql -uroot -proot test_21b < /script/load_data.sql"
 
 # Extract data grouped by tag_id
-docker exec -it mysql /bin/bash -c "rm -f /var/lib/mysql-files/extract_data.csv || true"
-docker exec -it mysql /bin/bash -c "mysql -uroot -proot test_21b < /script/extract_data.sql"
-docker cp mysql:/var/lib/mysql-files/extract_data.csv extract_data.csv
+docker exec -it mysql-test-mil /bin/bash -c "rm -f /var/lib/mysql-files/extract_data.csv || true"
+docker exec -it mysql-test-mil /bin/bash -c "mysql -uroot -proot test_21b < /script/extract_data.sql"
+docker cp mysql-test-mil:/var/lib/mysql-files/extract_data.csv extract_data.csv
 
 # Index extracted data to solr
-docker cp extract_data.csv solr:/extract_data.csv
-docker exec -it solr /bin/bash -c "bin/post -c tag_user_view /extract_data.csv"
+docker cp extract_data.csv solr-test-mil:/extract_data.csv
+docker exec -it solr-test-mil /bin/bash -c "bin/post -c tag_user_view /extract_data.csv"
 
 echo -e "\nTo see the recommendation, go to:"
 echo -e "\n\thttp://localhost:8983/solr/tag_user_view/mlt?q=ff0d3fb21c00bc33f71187a2beec389e9eff5332"
